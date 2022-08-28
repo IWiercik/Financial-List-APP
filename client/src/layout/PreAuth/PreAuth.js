28bfbe52034066cde3750c98e0da3b0154cd5715
Swiatlon <wiercikdoc@gmail.com>
2022-08-29 00:16:43 +0200

    Added routes

---
+++ client/src/layout/PreAuth/PreAuth.js
@@ -0,0 +1,28 @@
+import { PreAuthWrapper } from './PreAuth.style';
+import Navigation from 'components/organisms/Navigation/Navigation';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CenterContainer } from 'components/molecules/CenterContainer/CenterContainer.style';
+import Home from 'routes/Home/Home';
+import Login from 'routes/Login/Login';
+import Registration from 'routes/Registration/Registration';
+import PageNotFound from 'routes/PageNotFound/PageNotFound';
+
+function PreAuth() {
+  return (
+    <BrowserRouter>
+      <PreAuthWrapper>
+        <Navigation></Navigation>
+        <CenterContainer>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </CenterContainer>
+      </PreAuthWrapper>
+    </BrowserRouter>
+  );
+}
+
+export default PreAuth;
