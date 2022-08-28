586fe7411b890dc65fb0510aaa41d41201173c54
Swiatlon <wiercikdoc@gmail.com>
2022-08-29 00:10:14 +0200

    Initial view

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'assets/theme/theme';
 import { globalStyle as GlobalStyle } from 'assets/styles/globalStyle';
+import PreAuth from 'layout/PreAuth/PreAuth';
+import MainTemplate from 'layout/MainTemplate/MainTemplate';
+import { RootContainer } from 'Root.style';
 const Root = () => {
   const [serverData, setServerData] = useState([]);
-
   useEffect(() => {
     fetch('/api')
       .then((response) => response.json())
@@ -12,19 +14,12 @@ const Root = () => {
         setServerData(data.users);
       });
   }, []);
-
+  console.log(serverData);
+  const userIsAuthorized = false;
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle></GlobalStyle>
-      <div>
-        {typeof serverData === 'undefined' ? (
-          <p>Loading...</p>
-        ) : (
-          serverData.map((user, index) => {
-            return <p key={index}>{user}</p>;
-          })
-        )}
-      </div>
+      <RootContainer>{userIsAuthorized ? <MainTemplate /> : <PreAuth />}</RootContainer>
     </ThemeProvider>
   );
 };
