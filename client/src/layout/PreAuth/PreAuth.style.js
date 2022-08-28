4bfc5c5c390bd4623ace1cd66f79f82e1920f2bd
Swiatlon <wiercikdoc@gmail.com>
2022-08-29 00:16:57 +0200

    Styles for PreAuth

---
+++ client/src/layout/PreAuth/PreAuth.style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import backgroundIMG from 'assets/images/background.jpg';
+export const PreAuthWrapper = styled.div`
+  background: url(${backgroundIMG});
+  background-position: top;
+  background-repeat: no-repeat;
+  display: grid;
+`;
