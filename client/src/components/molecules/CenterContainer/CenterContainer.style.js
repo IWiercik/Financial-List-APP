937dc9d55ddf0749efb8215717de0b3f09a752b3
Swiatlon <wiercikdoc@gmail.com>
2022-08-30 19:13:01 +0200

    Added style for container

--- client/src/components/molecules/CenterContainer/CenterContainer.style.js
+++ client/src/components/molecules/CenterContainer/CenterContainer.style.js
@@ -4,4 +4,8 @@ export const CenterContainer = styled.div`
   justify-self: center;
   position: relative;
   min-width: 300px;
+  background: #000000;
+  border-radius: 25px;
+  border: 1px solid #0092c8;
+  box-shadow: 0px 0px 20px #046ee8;
 `;
