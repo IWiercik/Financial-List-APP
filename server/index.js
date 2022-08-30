db8a32d2bffa6cf02e10a687967bc99f99c8ffb3
Swiatlon <wiercikdoc@gmail.com>
2022-08-30 19:15:46 +0200

    Added listening to hear on posting data from client side

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
 app.get('/api', (req, res) => {
   res.json({ users: ['Hello', 'FullStack', 'Developer'] });
+});
+
+app.post('/Registration', (req, res) => {
+  console.log(req.body);
+  res.end();
+});
+app.post('/Login', (req, res) => {
+  console.log(req.body);
+  res.end();
 });
 
 app.listen(PORT, () => {
