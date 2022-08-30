41a77de8686c1f8fccfcb9a6e03016fce0df8311
Swiatlon <wiercikdoc@gmail.com>
2022-08-30 19:14:27 +0200

    Added initial version Login component

--- client/src/routes/Login/Login.js
+++ client/src/routes/Login/Login.js
@@ -1,5 +1,24 @@
+import Form from 'components/organisms/Form/Form';
+import { useState } from 'react';
+import axios from 'axios';
 function Login() {
-  return <div>Login</div>;
+  const initialState = { login: '', password: '' };
+  const [loginFormValues, setLoginFormValues] = useState(initialState);
+  const loginFormSubmitted = (e) => {
+    axios.post('http://localhost:5000/Login', { loginFormValues }).then((response) => {
+      console.log('[Axios Result]', response);
+      alert('Axios Working!');
+    });
+    e.preventDefault();
+  };
+  return (
+    <Form
+      title={'Login'}
+      value={loginFormValues}
+      updateValuesMethod={setLoginFormValues}
+      formSubmitted={loginFormSubmitted}
+    ></Form>
+  );
 }
 
 export default Login;
