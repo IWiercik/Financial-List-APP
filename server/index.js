9cc00fc7fd8242ae5fec17a8e4d5d39af7d0ecdb
Swiatlon <wiercikdoc@gmail.com>
2022-09-01 17:30:05 +0200

    Added functioning of creating and login user

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
@@ -10,17 +11,54 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+mongoose.connect('mongodb://localhost:27017/usersDB');
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model('User', userSchema);
+
 app.get('/api', (req, res) => {
   res.json({ users: ['Hello', 'FullStack', 'Developer'] });
 });
 
 app.post('/Registration', (req, res) => {
-  console.log(req.body);
-  res.end();
+  const newUser = new User({
+    email: req.body.login,
+    password: req.body.password,
+  });
+  newUser.save(function (err) {
+    let respondMessage;
+    if (err) {
+      respondMessage = 'Something went wrong!';
+    } else {
+      respondMessage = 'Succesfully created user!';
+    }
+    res.send(respondMessage);
+  });
 });
 app.post('/Login', (req, res) => {
-  console.log(req.body);
-  res.end();
+  const email = req.body.login;
+  const password = req.body.password;
+  User.findOne({ email: email }, function (err, foundUser) {
+    let respondMessage;
+    if (err) {
+      respondMessage = 'Something went wrong!';
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          respondMessage = 'Succesfully logged in!';
+        } else {
+          respondMessage = 'Check your email or password.';
+        }
+      } else {
+        respondMessage = 'Check your email or password.';
+      }
+    }
+    res.send(respondMessage);
+  });
 });
 
 app.listen(PORT, () => {
