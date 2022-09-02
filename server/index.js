b1a8b4e94f0d939c43f1cf3eb62570f79af7e410
Swiatlon <wiercikdoc@gmail.com>
2022-09-02 20:58:13 +0200

    Added custom errorHandler

--- server/index.js
+++ server/index.js
@@ -1,22 +1,32 @@
+require('dotenv').config();
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
-const app = express();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 5000;
 
-// app.use(bodyParser.json());
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(logger);
+
+app.use(express.json());
+
+app.use(cookieParser());
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/usersDB');
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
 
 const User = new mongoose.model('User', userSchema);
 
@@ -25,41 +35,58 @@ app.get('/api', (req, res) => {
 });
 
 app.post('/Registration', (req, res) => {
-  const newUser = new User({
-    email: req.body.login,
-    password: req.body.password,
-  });
-  newUser.save(function (err) {
+  const promise = new Promise((resolve, reject) => {
     let respondMessage;
-    if (err) {
-      respondMessage = 'Something went wrong!';
-    } else {
-      respondMessage = 'Succesfully created user!';
-    }
-    res.send(respondMessage);
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+      const newUser = new User({
+        email: req.body.login,
+        password: hash,
+      });
+      newUser.save(function (err) {
+        if (err) {
+          respondMessage = 'Something went wrong!';
+        } else {
+          respondMessage = 'Succesfully created user!';
+        }
+      });
+    });
+    // .then(() => {
+    //   console.log('koniec');
+    // });
   });
+  // promise.then(() => {
+  //   res.send(respondMessage);
+  // });
 });
 app.post('/Login', (req, res) => {
   const email = req.body.login;
   const password = req.body.password;
+  console.log('[email]', email);
+  console.log('[password]', password);
+
   User.findOne({ email: email }, function (err, foundUser) {
     let respondMessage;
-    if (err) {
-      respondMessage = 'Something went wrong!';
-    } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          respondMessage = 'Succesfully logged in!';
-        } else {
-          respondMessage = 'Check your email or password.';
-        }
-      } else {
-        respondMessage = 'Check your email or password.';
-      }
-    }
+    // if (err) {
+    //   respondMessage = 'Something went wrong!';
+    // } else {
+    //   if (foundUser) {
+    //     console.log('[found User!]');
+    //     bcrypt.compare(password, foundUser.password, function (err, result) {
+    //       if (result === true) {
+    //         respondMessage = 'Succesfully logged in!';
+    //       } else {
+    //         respondMessage = 'Check your email or password.';
+    //       }
+    //     });
+    //   } else {
+    //     respondMessage = 'There is no user with this email!';
+    //   }
+    // }
     res.send(respondMessage);
   });
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
