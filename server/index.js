fb8c6efba4528ee490c60355f676c1b7b5c18ee1
Swiatlon <wiercikdoc@gmail.com>
2022-09-19 18:38:00 +0200

    Cleaned file, by moving code to other files

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
 const { logger } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
+const corsOptions = require('./config/corsOptions');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 app.use(logger);
@@ -19,72 +19,12 @@ app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors(corsOptions));
+
+app.use('/', require('./routes/api')); // Api Route
+app.use('/', require('./routes/preAuth/routes')); //post Routes
 
 mongoose.connect('mongodb://localhost:27017/usersDB');
-
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-});
-
-const User = new mongoose.model('User', userSchema);
-
-app.get('/api', (req, res) => {
-  res.json({ users: ['Hello', 'FullStack', 'Developer'] });
-});
-
-app.post('/Registration', (req, res) => {
-  const promise = new Promise((resolve, reject) => {
-    let respondMessage;
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-      const newUser = new User({
-        email: req.body.login,
-        password: hash,
-      });
-      newUser.save(function (err) {
-        if (err) {
-          respondMessage = 'Something went wrong!';
-        } else {
-          respondMessage = 'Succesfully created user!';
-        }
-      });
-    });
-    // .then(() => {
-    //   console.log('koniec');
-    // });
-  });
-  // promise.then(() => {
-  //   res.send(respondMessage);
-  // });
-});
-app.post('/Login', (req, res) => {
-  const email = req.body.login;
-  const password = req.body.password;
-  console.log('[email]', email);
-  console.log('[password]', password);
-
-  User.findOne({ email: email }, function (err, foundUser) {
-    let respondMessage;
-    // if (err) {
-    //   respondMessage = 'Something went wrong!';
-    // } else {
-    //   if (foundUser) {
-    //     console.log('[found User!]');
-    //     bcrypt.compare(password, foundUser.password, function (err, result) {
-    //       if (result === true) {
-    //         respondMessage = 'Succesfully logged in!';
-    //       } else {
-    //         respondMessage = 'Check your email or password.';
-    //       }
-    //     });
-    //   } else {
-    //     respondMessage = 'There is no user with this email!';
-    //   }
-    // }
-    res.send(respondMessage);
-  });
-});
 
 app.use(errorHandler);
 
