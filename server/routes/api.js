84cb9358cc0eab7d07ddf71ca60e29171a05d083
Swiatlon <wiercikdoc@gmail.com>
2022-10-15 13:04:10 +0200

    Deleted old code into new one (refactoring)

--- server/routes/api.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-const userController = require('../controllers/userController');
-router.get('/api', userController.getUserData);
-
-module.exports = router;
