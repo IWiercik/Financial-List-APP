c747c112cd33f1afd52aa5fa2da432d4bf4184be
Swiatlon <wiercikdoc@gmail.com>
2022-10-15 13:00:22 +0200

    Deleted commented code

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'assets/theme/theme';
 import { globalStyle as GlobalStyle } from 'assets/styles/globalStyle';
@@ -6,14 +5,6 @@ import PreAuth from 'layout/PreAuth/PreAuth';
 import MainTemplate from 'layout/MainTemplate/MainTemplate';
 import { RootContainer } from 'Root.style';
 const Root = () => {
-  const [serverData, setServerData] = useState([]);
-  // useEffect(() => {
-  //   fetch('/api')
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setServerData(data.users);
-  //     });
-  // }, []);
   const userIsAuthorized = false;
   return (
     <ThemeProvider theme={theme}>
