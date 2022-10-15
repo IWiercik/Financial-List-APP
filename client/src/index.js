a6b96be027e5d6b4cfbe3f0ca51c0f8cd690b934
Swiatlon <wiercikdoc@gmail.com>
2022-10-15 13:00:04 +0200

    Added initial form of redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Root from 'Root';
+import { store } from 'redux/store/store';
+import { Provider } from 'react-redux';
+import { fetchUsers } from 'redux/slices/usersSlice';
+
+store.dispatch(fetchUsers());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Root />
+    <Provider store={store}>
+      <Root />
+    </Provider>
   </React.StrictMode>
 );
