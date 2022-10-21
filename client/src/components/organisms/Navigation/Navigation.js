da4a8546b4f850b0c9f03111572ec9f72ca3b391
Swiatlon <wiercikdoc@gmail.com>
2022-10-21 15:48:06 +0200

    Added logout button with functioning

--- client/src/components/organisms/Navigation/Navigation.js
+++ client/src/components/organisms/Navigation/Navigation.js
@@ -7,7 +7,28 @@ import LogoutIcon from 'assets/images/icons/log-out.svg';
 import { Icon } from 'components/atoms/Icon/Icon.style';
 import { Logo } from './Navigation.style';
 import { Logout } from './Navigation.style';
+import { useEffect } from 'react';
+import { selectCurrentToken } from 'redux/auth/authSlice';
+import { useSelector } from 'react-redux';
+
+import { useSendLogoutMutation } from 'redux/auth/authApiSlice';
 function Navigation() {
+  const isUserLogged = useSelector(selectCurrentToken);
+  const [sendLogout, { isLoading, isSuccess, isError, error }] = useSendLogoutMutation();
+  useEffect(() => {
+    if (isSuccess) console.log(isSuccess);
+  }, [isSuccess]);
+  const onLogoutClicked = async (e) => {
+    // console.log(isUserLogged);
+
+    // console.log(isUserLogged);
+    sendLogout();
+  };
+  const logoutButton = isUserLogged && (
+    <Logout onClick={onLogoutClicked}>
+      <Icon src={LogoutIcon}></Icon>Logout
+    </Logout>
+  );
   return (
     <Navbar>
       <NavbarList>
@@ -24,10 +45,8 @@ function Navigation() {
           <Icon src={RegisterIcon}></Icon>
           <p>Register</p>
         </NavbarListItem>
-        {/* <Logout>
-          <Icon src={LogoutIcon}></Icon>Logout
-        </Logout> */}
       </NavbarList>
+      {logoutButton}
     </Navbar>
   );
 }
