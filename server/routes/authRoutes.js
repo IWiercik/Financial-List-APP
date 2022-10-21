a8778b5499dd0c4e7d4f27d109ea7f71c0ab30c7
unknown <wiercikdoc@gmail.com>
2022-10-21 09:51:47 +0200

    Added JWT to routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/usersController');
-const loginLimiter = require('../middleware/loginLimiter');
+const authController = require("../controllers/authController");
+const loginLimiter = require("../middleware/loginLimiter");
+const verifyJWT = require("../middleware/verifyJWT");
 
-router.route('/').post(loginLimiter, authController.login);
+router.use(verifyJWT);
 
-router.route('/refresh').get(authController.refresh);
+router.route("/").post(loginLimiter, authController.login);
 
-router.route('/logout').post(authController.logout);
+router.route("/refresh").get(authController.refresh);
+
+router.route("/logout").post(authController.logout);
 module.exports = router;
