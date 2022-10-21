e872ff2638f9e4465a062f8c8df4c26062f801be
Swiatlon <wiercikdoc@gmail.com>
2022-10-21 15:49:36 +0200

    Refactored code

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,16 +6,16 @@ const asyncHandler = require('express-async-handler');
 //@desc Login
 //@route POST /auth
 const login = asyncHandler(async (req, res) => {
-  const { email, pswd: password } = req.body;
+  const { email, password } = req.body;
   if (!email || !password) {
     return res.status(400).json({ message: 'All fields are required!' });
   }
   const foundUser = await User.findOne({ email }).exec();
-  if (!foundUser || !foundUser) {
-    return res.status(401).json({ message: 'Unauthorized!' });
+  if (!foundUser) {
+    return res.json({ message: 'Unauthorized! by not found user' });
   }
   const match = await bcrypt.compare(password, foundUser.password);
-  if (!match) return res.status(401).json({ message: 'Unauthorized!' });
+  if (!match) return res.status(401).json({ message: 'Unauthorized! by password' });
   const accessToken = jwt.sign(
     {
       UserInfo: {
@@ -27,7 +27,7 @@ const login = asyncHandler(async (req, res) => {
   );
 
   const refreshToken = jwt.sign({ email: foundUser.email }, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: '1d',
+    expiresIn: '7d',
   });
 
   // Create secure cookie with refresh token
@@ -46,14 +46,14 @@ const login = asyncHandler(async (req, res) => {
 //@route GET /auth/refresh
 const refresh = (req, res) => {
   const cookies = req.cookies;
-  if (!cookies?.jwt) return res.status(401).json({ message: 'Unathorized!' });
+  if (!cookies?.jwt) return res.status(401).json({ message: 'Unathorized! by cookie' });
   const refreshToken = cookies.jwt;
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, async (err, decoded) => {
     if (err) return res.status(403).json({ message: 'Forbidden' });
 
     const foundUser = await User.findOne({ email: decoded.email }).exec();
 
-    if (!foundUser) return res.status(401).json({ message: 'Unauthorized' });
+    if (!foundUser) return res.status(401).json({ message: 'Unauthorized by JWT not found user' });
 
     const accessToken = jwt.sign(
       {
@@ -71,6 +71,7 @@ const refresh = (req, res) => {
 //@desc Logout
 //@route POST /auth/logout
 const logout = (req, res) => {
+  console.log('User logged off');
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(204); //No content
   res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
